f19e4e98a51f2b512544ac994b7af85d669648ed
Virgilio-D-Cabading-Jr <Virgilio.D.Cabading.Jr@gmail.com>
2021-10-01 22:07:15 -0700

    Uploading Java Script Prediction Code
    prediction for the code is in the comments

---
+++ Cabading_Virgilio_JSPredict_2021.10.01.2150.js
@@ -0,0 +1,27 @@
+// Code: Coding Dojo Pre-Bootcamp > Intro to Programming > Javascript > JS Predict
+// By: Virgilio.D.Cabading.Jr
+
+// Using comments, Answer the predicted questions
+
+// Predict 1: What will console.log state when this function is called?
+function myBirthYearFunc(){
+    console.log("I was born in " + 1980); //I was born in 1980
+}
+
+// Predict 2:  if the variable birthYearInput is 1980 
+//             (aka var birthYearInput = 1980), 
+//             and this function is called, what would the console.log state?
+function myBirthYearFunc(birthYearInput){
+    console.log("I was born in " + birthYearInput); //I was born in 1980
+}
+
+// Predict 3:  If var num1 = 10 and var num2 = 20
+//             what would the console.log state?
+function add(num1, num2){
+    console.log("Summing Numbers!"); //Summing Numbers!
+    console.log("num1 is: " + num1); //num1 is: 10
+    console.log("num2 is: " + num2); //num2 is: 20
+    
+    var sum = num1 + num2;
+    console.log(sum); //30
+}    
