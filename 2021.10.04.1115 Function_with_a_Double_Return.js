9572404f651780ab3883553c0ffd9b0dd9fbcde3
Virgilio-D-Cabading-Jr <Virgilio.D.Cabading.Jr@gmail.com>
2021-10-04 11:31:29 -0700

    Created a function with two returns

---
+++ 2021.10.04.1115 Function_with_a_Double_Return.js
@@ -0,0 +1,18 @@
+/////////////////////////////////////////////////
+// Coding Dojo > Pre-Bootcamp > Programming Basics > Intro to Algorithms
+// Function with a Double Return
+// By: Virgilio D. Cabading
+// Created: October 4, 2021
+/////////////////////////////////////////////////
+
+// This is a program to see what happen when you run a function with two returns
+
+function a()          // Created function a
+{
+    return 25;
+    return 50;
+}
+
+console.log(a());     // Execute the function
+
+// what I learned here: when a function returns, it ignores the rest of the code in the function after the return
